78d61f621200cf7a4229e2702b8ec57d449e9199
Mark Oliver <mark.c.oliver@gmail.com>
2019-03-10 11:30:19 -0400

    done

--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -25,9 +25,21 @@
   Create a cuboid object that uses the new keyword to use our CuboidMaker constructor
   Add properties and values of length: 4, width: 5, and height: 5 to cuboid.   
 */
+function CuboidMaker (length, width, height) {
+  this.length = length
+  this.width = width
+  this.height = height
+}
+CuboidMaker.prototype.volume = function () {
+  return this.length * this.width * this.height
+}
+CuboidMaker.prototype.surfaceArea = function () {
+  return 2 * (this.length * this.width + this.length * this.height + this.width * this.height)
+}
+cuboid = new CuboidMaker(4, 5, 5)
 
 // Test your volume and surfaceArea methods by uncommenting the logs below:
-// console.log(cuboid.volume()); // 100
-// console.log(cuboid.surfaceArea()); // 130
+console.log(cuboid.volume()); // 100
+console.log(cuboid.surfaceArea()); // 130
 
 
